13870cd1abce32331215e65b7fa5e6c3da0c49ea
Govindaraju <govindaraju_venugopal@apple.com>
2019-01-12 22:12:53 +0530

    first commit

---
+++ src/main/java/com/friends/comp/HerokuSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.friends.comp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/friends/comp/HerokuSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.friends.comp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HerokuSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
